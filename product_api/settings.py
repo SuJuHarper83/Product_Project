1d799e149f7204c359beb62491e3c0eeeeb25ba3
Your Name <you@example.com>
2022-08-01 20:44:55 -0400

    Created products app + changed database settings

--- product_api/settings.py
+++ product_api/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gzktswb4hcmim1$zq-p5_+*%6e6pzla$0&!+1-mj2mt+=%8-('
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
